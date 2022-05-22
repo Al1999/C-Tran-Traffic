885a5b5a35e61969bfa9383c71be304a53e0926f
Al <49578911+Al1999@users.noreply.github.com>
2022-05-22 03:33:21 -0700

    fixed the issue with producing speed values

--- tsvscript.py
+++ tsvscript.py
@@ -1,30 +1,33 @@
+
 #!/usr/bin/python3
 import csv, json
 from geojson import Feature, FeatureCollection, Point
 features = []
 
-with open('data.tsv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
+with open('data5_1.tsv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter='\t')
     data = csvfile.readlines()
     for line in data[1:len(data)-1]:
         line.strip()
-        row = line.split(",")
+        row = line.split("\t")
         
         # skip the rows where speed is missing
         print(row)
         x = row[0]
         y = row[1]
         speed = row[2]
+
+    
         if speed is None or speed == "":
             continue
-     
+
         try:
             latitude, longitude = map(float, (y, x))
             features.append(
                 Feature(
                     geometry = Point((longitude,latitude)),
                     properties = {
-                        'speed': (int(speed))
+                        'speed': (float(speed))
                     }
                 )
             )
@@ -32,5 +35,5 @@ with open('data.tsv', newline='') as csvfile:
             continue
 
 collection = FeatureCollection(features)
-with open("data.geojson", "w") as f:
+with open("data5_1.geojson", "w") as f:
     f.write('%s' % collection)
